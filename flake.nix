b53f22f5d09dcaef014a659ef0d9c227d0ae19cb
beeaniebee <whattheheccamidoing@pm.me>
2023-07-24 14:19:54 -0400

    home-manager module instead

--- flake.nix
+++ flake.nix
@@ -27,16 +27,5 @@
         modules = [ ./nixos/configuration.nix ];
       };
     };
-
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      "beanie@nixos" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-        # > Our main home-manager configuration file <
-        modules = [ ./home-manager/home.nix ];
-      };
-    };
   };
 }
