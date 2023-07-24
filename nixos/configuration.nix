04ad422fb21ce21593ce7feae4957818ac5c52ca
beeaniebee <whattheheccamidoing@pm.me>
2023-07-24 13:57:58 -0400

    first config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -69,7 +69,7 @@
   services.printing.enable = true;
 
   services.xserver.enable = true;
-  services.xserver.sddm.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
   services.xserver.libinput.enable = true;
 
