fea473a1b1d30420f0a09684402bd14e112d6be2
beeaniebee <whattheheccamidoing@pm.me>
2024-06-18 00:09:07 -0400

    more h-m fixes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,23 @@
-{ inputs, lib, config, pkgs, ... }: {
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other NixOS modules here
   imports = [
+    # If you want to use modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
-    inputs.home-manager.nixosModules.home-manager
   ];
 
   nixpkgs = {
