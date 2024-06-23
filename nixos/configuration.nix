c0d721ee14051770f48b25ccd53624faa3ea86cb
beeaniebee <whattheheccamidoing@pm.me>
2024-06-23 02:29:56 -0400

    disable plasma6

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -73,7 +73,7 @@
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = false;
 
   programs.hyprland.enable = true;
   programs.zsh.enable = true;
