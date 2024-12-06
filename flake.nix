fa52b81b1b19240e0b40445c64db88c3c0e4246a
beeaniebee <29309688+beeaniebee@users.noreply.github.com>
2024-12-06 00:31:33 -0500

    update to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "beanie's nixos flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     stylix.url = "github:danth/stylix";
@@ -11,7 +11,7 @@
     pyprland.url = "github:hyprland-community/pyprland";
     nur.url = "github:nix-community/NUR";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -255,5 +255,5 @@
 	  ];
     };
 
-  system.stateVersion = "24.05";
+  system.stateVersion = "24.11";
 }
