c423a0e3811a97e06a6c5d4e30e14f12efcbc7f0
beeaniebee <29309688+beeaniebee@users.noreply.github.com>
2026-05-29 21:57:47 -0400

    pre-modularization change: move tpmnrl aliaas to system-level config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -325,7 +325,6 @@
         clean = "sudo nix-collect-garbage -d;nix-collect-garbage -d";
         lsgen = "sudo nix-env --list-generations";
         delgen = "sudo /run/current-system/bin/switch-to-configuration boot";
-        tpmnrl = "sudo systemd-cryptenroll --tpm2-device=auto /dev/nvme0n1p6 --tpm2-pcrs=0+1+2+3+4+7 --wipe-slot=tpm2 --tpm2-with-pin=yes"; # && sudo systemd-cryptenroll --tpm2-device=auto /dev/nvme0n1p6 --tpm2-pcrs=0+1+2+3+4+5+6+7+8";
       };
       syntaxHighlighting.enable = true;
       autocd = true;
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -174,6 +174,10 @@
     git.enable = true;
   };
 
+  environment.shellAliases = {
+    tpmnrl = "sudo systemd-cryptenroll --tpm2-device=auto /dev/nvme0n1p6 --tpm2-pcrs=0+1+2+3+4+7 --wipe-slot=tpm2 --tpm2-with-pin=yes";
+  };
+
   environment.systemPackages = with pkgs; [
     vim
     wget
